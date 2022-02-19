555271e18984468eaf8c402896540a0a8345bc71
Lecogoni <giraud.nicolas@me.com>
2022-02-19 08:54:26 +0000

    set Fixtures and create a property fixtures

--- src/DataFixtures/PropertyFixtures.php
+++ src/DataFixtures/PropertyFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Property;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,11 +10,24 @@ class PropertyFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        
+      for ($i = 0; $i < 10; $i++) {
         $property = new Property();
+        $property->setTitle('mon bien '. $i);
+        $property->setDescription('description de mon bien '. $i);
+        $property->setSurface(mt_rand(10, 100));
+        $property->setRooms(mt_rand(1, 10));
+        $property->setBedrooms(mt_rand(1, 8));
+        $property->setFloor(mt_rand(1, 6));
+        $property->setPrice(mt_rand(50000, 200000));
+        $property->setHeat(1);
+        $property->setCity('montpellier');
+        $property->setAddress($i . ' rue du test');
+        $property->setSold(0);
+        $property->setPostalCode('34000');
         
-        $manager->persist($product);
-        $manager->flush();
+        
+        $manager->persist($property);
+     }
+    $manager->flush();
     }
 }
