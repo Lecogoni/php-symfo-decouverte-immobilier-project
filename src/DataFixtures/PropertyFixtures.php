4a4203975a7940fb81a7fbe98480f7f257defe5e
Lecogoni <giraud.nicolas@me.com>
2022-02-19 09:18:46 +0000

    install Faker and populate property fixtures with some faker data

--- src/DataFixtures/PropertyFixtures.php
+++ src/DataFixtures/PropertyFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Property;
+use Faker;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,20 +11,23 @@ class PropertyFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
+      $faker = Faker\Factory::create('fr_FR');
+
       for ($i = 0; $i < 10; $i++) {
         $property = new Property();
-        $property->setTitle('mon bien '. $i);
-        $property->setDescription('description de mon bien '. $i);
+        $property->setTitle('mon bien ' . $faker->word . ' ' . $i);
+        $property->setDescription('desription de mon bien ' .  $i);
         $property->setSurface(mt_rand(10, 100));
         $property->setRooms(mt_rand(1, 10));
         $property->setBedrooms(mt_rand(1, 8));
         $property->setFloor(mt_rand(1, 6));
         $property->setPrice(mt_rand(50000, 200000));
         $property->setHeat(1);
-        $property->setCity('montpellier');
-        $property->setAddress($i . ' rue du test');
+        $property->setCity($faker->city);
+        $property->setAddress($i . ' ' . $faker->streetName);
         $property->setSold(0);
-        $property->setPostalCode('34000');
+        $property->setPostalCode($faker->postcode);
         
         
         $manager->persist($property);
